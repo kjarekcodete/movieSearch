ded562dc6e5fcaf7d961d997d54b887ab88cd40c
Konrad Jarek <konrad.jarek@i22.de>
2020-05-18 15:48:56 +0200

    Updates

---
+++ precache-manifest.292fd76e191ffdaf05fc1305e66543fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8503bf8a9dbb25ae899fb81e037015",
+    "url": "/movieSearch/index.html"
+  },
+  {
+    "revision": "7cd037b14dc445c3eb1d",
+    "url": "/movieSearch/static/css/main.13064abe.chunk.css"
+  },
+  {
+    "revision": "0c8e566a8a544e98d7e6",
+    "url": "/movieSearch/static/js/2.37dc21ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieSearch/static/js/2.37dc21ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd037b14dc445c3eb1d",
+    "url": "/movieSearch/static/js/main.2ed1139e.chunk.js"
+  },
+  {
+    "revision": "ab59ddada2e28017d786",
+    "url": "/movieSearch/static/js/runtime-main.8314b522.js"
+  }
+]);
