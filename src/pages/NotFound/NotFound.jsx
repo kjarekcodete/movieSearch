0f1be358cc8775d6ecc20b42f6cecc40a2511cdb
Konrad Jarek <konrad.jarek@i22.de>
2020-05-18 17:01:16 +0200

    deploy

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -5,8 +5,7 @@ import './NotFound.scss'
 const NotFound = () => {
   return <section className="container not-found dark-background">
     <p className="not-found-info">Unfortunately we cannot find page which you are looking for.
-      Please use Back button to go to search page
-      to search page</p>
+      Please use Back button to go to search page</p>
     <BackButton path={'/'}/>
   </section>
 }
