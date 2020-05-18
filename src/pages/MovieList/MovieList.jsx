450ec2816101adfc4b2012122388a87aaa961d2c
Konrad Jarek <konrad.jarek@i22.de>
2020-05-18 15:47:15 +0200

    githubPage

--- src/pages/MovieList/MovieList.jsx
+++ src/pages/MovieList/MovieList.jsx
@@ -53,7 +53,4 @@ const MovieList = () => {
 
 
 
-
-
-
 export default MovieList;
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,7 +4,9 @@ import './NotFound.scss'
 
 const NotFound = () => {
   return <section className="container not-found dark-background">
-    <p className="not-found-info">Unfortunately we cannot find page which you looking for please use Back button to go to search page</p>
+    <p className="not-found-info">Unfortunately we cannot find page which you are looking for.
+      Please use Back button to go to search page
+      to search page</p>
     <BackButton path={'/'}/>
   </section>
 }
