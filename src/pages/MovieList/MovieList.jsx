f9f691e3cef9cc8ced37f1f66f0dbfab01c4869f
Konrad Jarek <konrad.jarek@i22.de>
2020-05-18 14:13:07 +0200

    add loader to movielist

--- src/pages/MovieList/MovieList.jsx
+++ src/pages/MovieList/MovieList.jsx
@@ -11,7 +11,8 @@ import './MovieList.scss';
 
 const MovieList = () => {
     const {movies, updateMovies} = useContext(MovieContext);
-    const {history} = useHistory();
+    const [loader, setLoader] = useState(false);
+    const history = useHistory();
     const {title, year} = useParams();
     const [results, setResults] = useState([]);
     let titleKey = year ? `${title}/${year}` : title;
@@ -21,8 +22,10 @@ const MovieList = () => {
             setResults(movies[titleKey])
         } else {
             try {
+                setLoader(true);
                 const {data} = await getMovies(title, year);
                 const searchResult = data.Search.filter(el => el.Type !== 'game');
+                setLoader(false);
                 setResults(searchResult)
                 const newMovie = {...movies};
                 newMovie[titleKey] = searchResult;
@@ -31,14 +34,20 @@ const MovieList = () => {
                 history.push('/notFound')
             }
         }
-    }, [updateMovies, movies, history])
+    }, [updateMovies, movies, history, setLoader])
 
     useEffect(() => { checkIfExist(title, titleKey, year) }, [title, titleKey, year, checkIfExist])
 
     return <section className='container list-page'>
         <PageHeader customClassName={"dark-background"} title={`You serched ${title}`}/>
         <main className='list-page-results'>
-            { results && results.map(el =>  <MovieTile key={el.imdbID} {...el}/>)}
+            {loader ? <div className="spinner-border text-light text-center" role="status">
+                  <span className="sr-only">Loading...</span>
+              </div> :
+              (results && results.map(el => <MovieTile key={el.imdbID} {...el}/>))
+            }
+
+
         </main>
 
     </section>
